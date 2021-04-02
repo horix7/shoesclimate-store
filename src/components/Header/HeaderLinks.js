e9fbb2ef00866310a5651e1cc2fa0662a3972e61
horix7 <horix77777@gmail.com>
2021-04-02 14:08:42 +0200

    first commit

---
+++ src/components/Header/HeaderLinks.js
@@ -0,0 +1,123 @@
+/*eslint-disable*/
+import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+
+// @material-ui/icons
+import { Apps, CloudDownload } from "@material-ui/icons";
+
+// core components
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.js";
+import Button from "components/CustomButtons/Button.js";
+
+import styles from "assets/jss/material-kit-react/components/headerLinksStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function HeaderLinks(props) {
+  const classes = useStyles();
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          buttonText="COLLECTIONS"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent"
+          }}
+          dropdownList={[
+            <Link to="/" className={classes.dropdownLink}>
+               collections
+            </Link>,
+            <a
+              href="#"
+              target="_blank"
+              className={classes.dropdownLink}
+            >
+              shop 
+            </a>
+          ]}
+        />
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="#"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+           SHOP 
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="account"
+          title="view Your Account Details"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="#"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fas fa-user"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="search"
+          title="Search Products"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="#"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fas fa-search"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+
+      <ListItem className={classes.listItem}>
+        {/*<Tooltip title="Delete">
+          <IconButton aria-label="Delete">
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>*/}
+        <Tooltip
+          id="shop"
+          title="view Your Cart"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            href="#"
+            target="_blank"
+            color="transparent"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fas fa-shopping-cart"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+   
+    
+    </List>
+  );
+}
