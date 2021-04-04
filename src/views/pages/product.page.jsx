70083a30893566ad86413321d679ad49923225cb
horix7 <horix77777@gmail.com>
2021-04-04 11:44:49 +0200

    modified Product Page

--- src/views/pages/product.page.jsx
+++ src/views/pages/product.page.jsx
@@ -28,14 +28,31 @@ export default class ProductPage extends Component {
                             157,000 RWF 
                         </Typography>
                         <FormControl>
-                        <InputLabel id="label" style={{margin:  "5px"}}>  Size </InputLabel>
+                        <p className="product-label"> Size    </p>
                          <Select labelId="label" variant="outlined" id="label" Label="Size" className="product-select">
                             <MenuItem > 230 </MenuItem>
                             <MenuItem > 230 </MenuItem>
                         </Select>
                         </FormControl>
 
+                        <p className="product-label"> Quantinty    </p>
+                        <div className="counterForm">
+                       <div className="count-i">
+                       <i width="100%" className="fas fa-plus"></i>
+                       </div>
+                       <div className="count-i">
+                        10
+                       </div>
+                       <div className="count-i">
+                        <i width="100%" className="fas fa-minus"></i>
+                        </div>
+                        </div>
+
                         <button className="cart-button"> Add To cart </button>
+
+                        <Typography className="product-description">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, aliquam harum perferendis sit incidunt odit alias beatae mollitia exercitationem velit eaque quidem laboriosam natus nulla tenetur, accusantium provident, unde excepturi.
+                        </Typography>
                     </div>
                 </div>
 
