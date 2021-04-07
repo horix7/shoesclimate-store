6dc21d8295473eebbbe17978dd345c8fdb37db17
horix7 <horix77777@gmail.com>
2021-04-07 15:51:49 +0200

    create backend services calls

---
+++ src/services/productService.js
@@ -0,0 +1,37 @@
+import http from './http'
+
+
+export const getProductData = async () => {
+
+    const products = await http.get("/api/v1/products")
+    return products
+
+}
+
+export const getUserCart = async () => {
+
+    const cart = await http.get("/api/v1/cart")
+    return cart
+    
+}
+
+export const getUserOrders = async () => {
+
+    const orders = await http.get("/api/v1/orders")
+    return orders
+    
+}
+
+export const createOrder = async(newOrder) => {
+
+    const order = await http.post("/api/v1/orders", newOrder)
+    return order
+}
+
+
+export const createCartService = async(cart) => {
+
+    const createCart = await http.post("/api/v1/cart", cart)
+    return createCart
+}
+
