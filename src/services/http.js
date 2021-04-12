8a3154b43d64a8dfbbf8f4fded3fdc0d750a3b21
horix7 <horix77777@gmail.com>
2021-04-12 21:17:06 +0200

    modfied http address

--- src/services/http.js
+++ src/services/http.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-
-axios.defaults.baseURL = "http://172.20.10.2:4000";
+axios.defaults.baseURL = "http://206.189.232.135:4000";
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("AUTH_TOKEN")}`;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
