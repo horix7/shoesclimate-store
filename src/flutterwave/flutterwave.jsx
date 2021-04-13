8193388ad3abfebb252ea1aa10f9448767832be0
horix7 <horix77777@gmail.com>
2021-04-13 19:03:59 +0200

    adding payment

---
+++ src/flutterwave/flutterwave.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { useFlutterwave, closePaymentModal } from 'flutterwave-react-v3';
+import { TextField, Button } from "@material-ui/core"
+
+export default function FlutterWave () {
+  const [config, setConfig ] = useState({
+    public_key: 'FLWPUBK-**************************-X',
+    tx_ref: Date.now(),
+    amount: 100,
+    currency: 'NGN',
+    payment_options: 'card,mobilemoney',
+    customer: {
+      email: 'user@gmail.com',
+      phonenumber: '07064586146',
+      name: 'joel ugwumadu',
+    },
+    customizations: {
+      title: 'shoes climate',
+      description: 'Payment for items in cart',
+      logo: 'https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg',
+    },
+  })
+
+
+
+  const handleFlutterPayment = useFlutterwave(config);
+
+  return (
+    <div className="App">
+    
+    <TextField variant="outlined" label="email" type="email" />
+    <TextField variant="outlined" label="name" type="name" />
+    <TextField variant="outlined" label="phone" type="number" />
+
+      <Button
+        variant="contained"
+        onClick={() => {
+          handleFlutterPayment({
+            callback: (response) => {
+               console.log(response);
+                closePaymentModal() // this will close the modal programmatically
+            },
+            onClose: () => {},
+          });
+        }}
+      >
+        Contiue To Payment 
+      </Button>
+    </div>
+  );
+}
