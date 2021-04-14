9ea06f8618ac59b9c1502f22309e9589b6f333b7
horix7 <horix77777@gmail.com>
2021-04-14 11:34:14 +0200

    validating billing information

--- src/flutterwave/flutterwave.jsx
+++ src/flutterwave/flutterwave.jsx
@@ -7,42 +7,52 @@ export default function FlutterWave () {
     public_key: 'FLWPUBK_TEST-96bc0402de1213b618f9f58986fd0701-X',
     tx_ref: Date.now(),
     amount: 100,
-    currency: 'NGN',
+    currency: 'RWF',
     payment_options: 'card,mobilemoney',
     customer: {
-      email: 'user@gmail.com',
-      phonenumber: '0500px4586146',
-      name: 'joel ugwumadu',
+      email: null,
+      phonenumber: null,
+      name: null,
     },
     customizations: {
-      title: 'shoes climate',
+      title: 'SHOES-CLIMATE',
       description: 'Payment for items in cart',
       logo: 'https://pbs.twimg.com/profile_images/1298146343785988096/NKTCuQrE.jpg',
     },
   })
 
 
+  const handleClientInput = (event) => {
+    let payment_info = {...config}
+    payment_info.customer[event.target.id] = event.target.value
+
+    setConfig({...payment_info})
+  }
 
   const handleFlutterPayment = useFlutterwave(config);
 
   return (
     <div className="App">
     
-    <TextField variant="outlined" fullWidth style={{width: "500px"}} label="email" type="email" /> <br/> <br/>
-    <TextField variant="outlined" fullWidth style={{width: "500px"}} label="name" type="name" /> <br/> <br/>
-    <TextField variant="outlined" fullWidth style={{width: "500px"}} label="phone" type="number" /> <br/> <br/> 
+    <TextField variant="outlined" onChange={handleClientInput} fullWidth style={{width: "500px"}}  id="email" label="email" type="email" /> <br/> <br/>
+    <TextField variant="outlined" onChange={handleClientInput} fullWidth style={{width: "500px"}} id="name" label="name" type="name" /> <br/> <br/>
+    <TextField variant="outlined" onChange={handleClientInput} fullWidth style={{width: "500px"}} label="phone"   id="phonenumber" type="number" /> <br/> <br/> 
 
       <Button
         variant="contained"
         onClick={() => {
+          if(Object.values(config.customer).some(elem => elem == null)) {
+            alert("filling in your billing information please")
+          }else {
+
           handleFlutterPayment({
             callback: (response) => {
-               console.log(response);
-                closePaymentModal() // this will close the modal programmatically
+                closePaymentModal() 
             },
             onClose: () => {},
           });
-        }}
+        }
+      }}
       >
         Contiue To Payment 
       </Button>
