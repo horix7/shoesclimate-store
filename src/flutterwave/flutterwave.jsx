28cc92f4891bb27b8dd0c40374873a499e30394d
horix7 <horix77777@gmail.com>
2021-04-14 11:20:06 +0200

    fixed flutter plugin

--- src/flutterwave/flutterwave.jsx
+++ src/flutterwave/flutterwave.jsx
@@ -4,7 +4,7 @@ import { TextField, Button } from "@material-ui/core"
 
 export default function FlutterWave () {
   const [config, setConfig ] = useState({
-    public_key: 'FLWPUBK-**************************-X',
+    public_key: 'FLWPUBK_TEST-96bc0402de1213b618f9f58986fd0701-X',
     tx_ref: Date.now(),
     amount: 100,
     currency: 'NGN',
@@ -17,7 +17,7 @@ export default function FlutterWave () {
     customizations: {
       title: 'shoes climate',
       description: 'Payment for items in cart',
-      logo: 'https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg',
+      logo: 'https://pbs.twimg.com/profile_images/1298146343785988096/NKTCuQrE.jpg',
     },
   })
 
