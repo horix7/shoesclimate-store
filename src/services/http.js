f14f0f6e06a4009db11dd41f74962d0f56cfe410
horix7 <horix77777@gmail.com>
2021-04-15 21:07:38 +0200

    added modified

--- src/services/http.js
+++ src/services/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://206.189.232.135:4000";
+axios.defaults.baseURL = "https://shoesclimate.herokuapp.com";
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("AUTH_TOKEN")}`;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
