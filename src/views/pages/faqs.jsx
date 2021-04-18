e2a79d80b717b69f229aa9b89e41f76c201f4b54
horix7 <horix77777@gmail.com>
2021-04-18 19:07:35 +0200

    adding fotter pages

---
+++ src/views/pages/faqs.jsx
@@ -0,0 +1,35 @@
+import React, { Component, Fragment } from 'react'
+import Header from 'components/Header/Header'
+import HeaderLinks from 'components/Header/HeaderLinks'
+import LogoImg from 'assets/img/logo.png'
+import Footer from 'components/Footer/Footer'
+
+
+export default class Faqs extends Component {
+
+    render () {
+
+        return (
+            <Fragment>
+                  <Header
+                    brand={<Fragment> <img className="nav-logo" src={LogoImg} /> <span className="nav-text"> SHOES CLIMATE </span></Fragment>}
+                    rightLinks={<HeaderLinks />}
+                    fixed
+                    color="white"
+                    changeColorOnScroll={{
+                    height: 300,
+                    color: "white"
+                    }}
+                />
+                <div  className="size-chart" >
+
+                <h2> Faqs </h2>
+                <div>
+                </div>
+                </div>
+            
+                <Footer />
+            </Fragment>
+        )
+    }
+}
