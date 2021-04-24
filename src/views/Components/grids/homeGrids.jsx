7a338416756d11b525105bd7cd9beb7e737f5ae2
horix7 <horix77777@gmail.com>
2021-04-24 22:32:46 +0200

    collections responsiveness

--- src/views/Components/grids/homeGrids.jsx
+++ src/views/Components/grids/homeGrids.jsx
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import  ProductCard from "components/productCard/card";
 import { Button, Typography } from '@material-ui/core';
-import { ArrowRight } from "@material-ui/icons";
+import { ArrowRightAlt } from "@material-ui/icons";
 
 
 export default class ProductGrids extends Component {
@@ -15,7 +15,7 @@ export default class ProductGrids extends Component {
                     <p>  {this.props.products[0].collection }</p>
                     </Typography>
 
-                    <Button endIcon={<ArrowRight />} variant="" style={{backgroundColor: "transparent", color: "black", border: "none"}} href={"/"+ this.props.products[0].collection}> View All </Button>
+                    <Button endIcon={<ArrowRightAlt />} style={{color: "#535457"}} href={"/"+ this.props.products[0].collection}> View All </Button>
 
                     </div>
                   <div className="product_grids"> { this.props.products.map(elem => <ProductCard key={elem.id} product={elem} />)} </div> 
