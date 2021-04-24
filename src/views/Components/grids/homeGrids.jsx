e0a601318f46285ede39bdaf7658e0ecb8151ff1
horix7 <horix77777@gmail.com>
2021-04-24 19:13:33 +0200

    styling home page

--- src/views/Components/grids/homeGrids.jsx
+++ src/views/Components/grids/homeGrids.jsx
@@ -1,5 +1,8 @@
 import React, { Component, Fragment } from 'react'
 import  ProductCard from "components/productCard/card";
+import { Button, Typography } from '@material-ui/core';
+import { ArrowRight } from "@material-ui/icons";
+
 
 export default class ProductGrids extends Component {
    
@@ -7,6 +10,14 @@ export default class ProductGrids extends Component {
         return (
 
             <Fragment>
+                    <div className="category">
+                    <Typography>
+                    <p>  {this.props.products[0].collection }</p>
+                    </Typography>
+
+                    <Button endIcon={<ArrowRight />} variant="" style={{backgroundColor: "transparent", color: "black", border: "none"}} href={"/"+ this.props.products[0].collection}> View All </Button>
+
+                    </div>
                   <div className="product_grids"> { this.props.products.map(elem => <ProductCard key={elem.id} product={elem} />)} </div> 
             </Fragment>
         )
