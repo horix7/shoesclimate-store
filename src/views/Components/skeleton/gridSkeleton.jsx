51d6efb994dfc9a46e32d46237cbd15a912252b5
horix7 <horix77777@gmail.com>
2021-04-25 18:25:38 +0200

    deployment

--- src/views/Components/skeleton/gridSkeleton.jsx
+++ src/views/Components/skeleton/gridSkeleton.jsx
@@ -5,7 +5,7 @@ export default function Variants() {
   return (
     <div>
       <div style={{ padding: "10px"}}>
-      <Skeleton variant="rect" width={"100%"} height={200} />
+      <Skeleton variant="rect" width={"100%"} height={150} />
       <Skeleton variant="text" width={"90%"} style={{marginLeft: "5%"}}/>
       <Skeleton variant="text" width={"60%"} style={{marginLeft: "20%"}}  height={40}/>
       </div>
