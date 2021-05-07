fd3de18f77ee08f028d2ad6ecc1ae6cea7794492
horix7 <horix77777@gmail.com>
2021-05-07 20:41:18 +0200

    updating the images

--- src/views/pages/search-results.jsx
+++ src/views/pages/search-results.jsx
@@ -8,11 +8,10 @@ import Parallax from "components/Parallax/Parallax.js";
 import HeaderLinks from "components/Header/HeaderLinks.js";
 import styles from "assets/jss/material-kit-react/views/components.js";
 import ProductCoursel from "components/productCoursel/coursel";
-import SHowCaseCoursel from  'components/productCoursel/showcasecoursel'
+import SHowCaseCoursel from 'components/productCoursel/showcasecoursel'
 import Button from '@material-ui/core/Button'
 import LogoImg from '../../assets/img/logo.png'
 import ProductGrids from "../Components/grids/resultsGrids"
-import BackgroundImage from 'assets/img/bg4.jpg'
 import { searchProducts } from "../../services/productService";
 import GridSkeleton from '../Components/skeleton/gridSkeleton'
 
@@ -28,33 +27,33 @@ export default function SearchResults(props) {
 
     try {
       const products = await searchProducts(props.match.params.search)
-    setState(products.data)
-    }catch {
+      setState(products.data)
+    } catch {
       setState([])
     }
 
   }, state)
   return (
     <Fragment>
-            <Header
-                    brand={<Fragment> <img className="nav-logo" src={LogoImg} /> <span className="nav-text"> SHOES CLIMATE </span></Fragment>}
-                    rightLinks={<HeaderLinks />}
-                    fixed
-                    color="white"
-                    changeColorOnScroll={{
-                    height: 300,
-                    color: "white"
-                    }}
-                />
+      <Header
+        brand={<Fragment> <img className="nav-logo" src={LogoImg} /> <span className="nav-text"> SHOES CLIMATE </span></Fragment>}
+        rightLinks={<HeaderLinks />}
+        fixed
+        color="white"
+        changeColorOnScroll={{
+          height: 300,
+          color: "white"
+        }}
+      />
 
-     <div className="shopPage">
-       {state.length >= 1 ? 
-     <ProductGrids products={state} search={props.match.params.search} />
-        :  <div className="product_grids">
-        {[1,2,3,4,5,6,7,8,9,10,11,12].map(() => <GridSkeleton /> )}
-    </div> }
-     </div>
- <Footer />
+      <div className="shopPage">
+        {state.length >= 1 ?
+          <ProductGrids products={state} search={props.match.params.search} />
+          : <div className="product_grids">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(() => <GridSkeleton />)}
+          </div>}
+      </div>
+      <Footer />
     </Fragment>
   );
 }
